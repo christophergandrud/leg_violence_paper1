cb5be7f2507d8f673ccc574aeebeade00796fa8b
christophergandrud <christopher.gandrud@gmail.com>
2015-03-30 13:19:00 +0200

    minor

--- Data/gather_violence_merge.R
+++ Data/gather_violence_merge.R
@@ -23,3 +23,12 @@ newer <- main %>% get_via_csv(ws = 'Version2')
 # New ymd variable
 newer$date <- sprintf('%s-%s-%s', newer$year, newer$month, newer$day) %>% 
                 ymd
+
+
+#### Download pre-2012 version ####
+older_url <- 'https://raw.githubusercontent.com/christophergandrud/leg_violence_paper1/master/Data/raw/leg_data_pre_2012.csv'
+older <- import(older_url)
+
+# New ymd variable
+older$date <- sprintf('%s-%s-%s', older$Year, older$Month, older$Day) %>% 
+                    ymd
