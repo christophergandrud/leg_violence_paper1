78321817cbaf349beb8d5a1262d2ca711a7cadd5
christophergandrud <christopher.gandrud@gmail.com>
2015-03-30 14:02:27 +0200

    minor

--- Data/gather_violence_merge.R
+++ Data/gather_violence_merge.R
@@ -5,6 +5,9 @@
 # MIT License
 ##########################
 
+# Set working directory. Change as needed.
+setwd('/git_repositories/leg_violence_paper1/')
+
 # Load required packages
 if (!('googlesheets' %in% installed.packages()[, 1]))
     devtools::install_github('jennybc/googlesheets')
@@ -14,6 +17,8 @@ if (!('rio' %in% installed.packages()[, 1]))
 library(rio)
 library(dplyr)
 library(lubridate)
+library(countrycode)
+library(DataCombine)
 
 #### Download original googlesheets versions ###
 main <- register_ss('10cK1dmAoWgBLtayc3lFMKeLQ7IM_lq0MG1G61K5VeDo')
@@ -23,6 +28,10 @@ newer <- main %>% get_via_csv(ws = 'Version2')
 # New ymd variable
 newer$date <- sprintf('%s-%s-%s', newer$year, newer$month, newer$day) %>% 
                 ymd
+newer$iso2c <- countrycode(newer$country, origin = 'country.name', 
+                           destination = 'iso2c')
+
+newer_sub <- newer %>% select(iso2c, date, URL) %>% as.data.frame
 
 
 #### Download pre-2012 version ####
@@ -32,3 +41,29 @@ older <- import(older_url)
 # New ymd variable
 older$date <- sprintf('%s-%s-%s', older$Year, older$Month, older$Day) %>% 
                     ymd
+older$iso2c <- countrycode(older$Country, origin = 'country.name', 
+                           destination = 'iso2c')
+older <- older %>% dplyr::rename(URL = `Link to source`)
+
+older_sub <- older %>% select(iso2c, date, URL)
+
+#### Fill in Missing URLs ####
+comb <- FillIn(D1 = newer_sub, D2 = older_sub, Var1 = 'URL', Var2 = 'URL',
+               KeyVar = c('iso2c', 'date'))
+
+only_old <- anti_join(older_sub, comb)
+
+comb <- rbind(comb, only_old)
+
+comb$country <- countrycode(comb$iso2c, origin = 'iso2c', 
+                            destination = 'country.name')
+
+comb <- comb %>% MoveFront('country') %>% dplyr::rename(source_url = URL) %>%
+        arrange(country, date)
+
+# Drop US-Alabama 2007 fight as it is not a national legislature
+comb <- comb %>% DropNA('iso2c')
+
+#### Save 
+export(comb, 'Data/violence_sources.csv')
+
